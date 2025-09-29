4e7e438157c17fef54802072dda4d90b3f0e763f
pCarbonero <pcarboneroalmellones@gmail.com>
2025-09-29 18:45:54 +0200

    experience entity y datasource

--- resume_app/lib/data/api_datasource.dart
+++ resume_app/lib/data/api_datasource.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-
 import 'package:dartz/dartz.dart';
+import 'package:resume_app/data/entities/experience_entity.dart';
 import 'package:resume_app/data/entities/project_entity.dart';
+import 'package:resume_app/domain/entities/experience_model.dart';
 import 'package:resume_app/domain/entities/project_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:resume_app/domain/exceptions/failure.dart';
@@ -9,6 +10,7 @@ import 'package:resume_app/domain/exceptions/failure.dart';
 class ApiDatasource {
   static final String apiUrl = "10.0.2.2:5050"; 
   static final String projectsEndpoint = 'projects';  
+  static final String experienceEndpoint = 'experience';
 
   static Future<Either<Failure, List<ProjectModel>>> getAllProjects() async {
     try {
@@ -32,5 +34,30 @@ class ApiDatasource {
     catch (error) {
       return Left(DataSourceException(error.toString()));
     }
+  }
+
+
+  static Future<Either<Failure, List<ExperienceModel>>> getAllExperience(lang) async {
+    try{
+      final uri = Uri.http(apiUrl, projectsEndpoint);
+
+      var response = await http.get(uri); 
+
+      if (response.statusCode == 200) {
+        final List<dynamic> decodedJson = json.decode(response.body);
+
+        final experience = decodedJson
+            .map((item) => ExperienceEntity.fromMap(item).toModel())
+            .toList();
+
+        return Right(experience);
+      }
+      else {
+        return Left(DataSourceException("Error: ${response.statusCode}"));
+      }
+    }
+    catch(error){
+      return Left(DataSourceException("error"));
+    } 
   }
 }
---
+++ resume_app/lib/data/entities/experience_entity.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:resume_app/domain/entities/experience_model.dart';
+
+class ExperienceEntity {
+  final String companyName;
+  final String role;
+  final String date;
+  final String location;
+  final List<String> description;
+  final String companyLogo;
+
+  const ExperienceEntity({
+    required this.companyName,
+    required this.role,
+    required this.date,
+    required this.location,
+    required this.description,
+    required this.companyLogo
+  });
+
+  factory ExperienceEntity.fromJson(String str) => ExperienceEntity.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory ExperienceEntity.fromMap(Map<String, dynamic> json) => ExperienceEntity(
+    companyName: json["name"] ?? "",
+    role: json["role"] ?? "",
+    date: json["date"] ?? "",
+    location: json["location"] ?? "",
+    description: json["description"] ?? "",
+    companyLogo: json["companyLogo"] ?? ""
+  );
+
+  Map<String, dynamic> toMap() => {
+    "companyName": companyName,
+    "role": role,
+    "date": date,
+    "location": location,
+    "description": description,
+    "companyLogo": companyLogo
+  };
+}
+
+extension ExperienceEntityMapper on ExperienceEntity {
+  ExperienceModel toModel() {
+    return ExperienceModel(
+      companyName: companyName, 
+      role: role,
+      date: date,
+      location: location,
+      description: description, 
+      companyLogo: companyLogo
+    );
+  }
+}
